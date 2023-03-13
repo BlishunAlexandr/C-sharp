843558b3d7e803c205558921e0a6c536a9ba0738
Александр Блишун <[blishunaliaksandr@gmail.com]>
2023-03-13 19:47:49 +0300

    add home3 and home4

---
+++ home3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int i = 0;
+int two = 0;
+int One = 12321;
+int OneCheck = One;
+while(i < 5)
+{
+    two = (two * 10) + (One % 10);
+    One = One / 10;
+    i++;
+}
+
+if (OneCheck == two)
+{
+    Console.WriteLine($"число {OneCheck} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"число {OneCheck} не является палиндромом");
+}
+
+
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+ Console.Write("Какое кол-во раз надо возвести число в куб: ");
+int Check = int.Parse(Console.ReadLine() ?? "");
+i = 0;
+int Num = 1;
+while (i < Check) {
+    Console.WriteLine($"{Num} ** 3 = {Math.Pow(Num, 3)}");
+    i++;
+    Num++;
+}
---
+++ home4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int A = 3; 
+int A_clone = A;
+int B = 5;
+int i = 1;
+
+while(i < B)
+{
+    A = A * A_clone;
+    i++;
+}
+
+Console.WriteLine($"Итог : {A}");
+
+
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int Num = 5433435;
+int Result = 0;
+while(true)
+{
+    int Num_A = Num % 10;
+    Num = Num / 10;
+    Result = Result + Num_A;
+    if(Num % 10 == 0) break;
+}
+Console.WriteLine($"Получается {Result}");
+
+
+
